6413a6f13a7e3f9ce1d582ac0cdd7667b5e8bb38
Jona Marie Ranola <jonaxranola@yahoo.com>
2014-05-25 16:35:12 +0800

    Revert "Revert "ommit""
    This reverts commit e1b151acbbd9f4d714302217bc67c70e8d09ad93.

---
+++ index.php
@@ -0,0 +1,222 @@
+<?php
+?>
+
+<html lang=en">
+	<head>
+		<link href="http://s3.amazonaws.com/codecademy-content/courses/ltp/css/bootstrap.css" rel="stylesheet">
+		<link rel="stylesheet" href="main.css">
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
+		<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.js"></script>
+		<script type="text/javascript" src="http://creativecouple.github.io/jquery-timing/documentation.js?v=20130928-164118"></script>
+		<script type="text/javascript" src="http://creativecouple.github.io/jquery-timing/jquery-timing.min.js?v=20130928-164118"></script>
+		<script type="text/javascript" src="javashoot.js"></script>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<link rel="shortcut icon" href="img/icon.png">
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Jona Ranola</title>
+	</head>
+
+	<body>
+ <body>
+
+    <div class="jumbotron">
+      <div class="container">
+      	<center><h1><div class="name">Jona Rañola</div></h1>
+      	<hr />
+			<div class="row">
+				<div class="col-md-3"> </div>
+		  	  	<div class="col-md-6">
+					<a href="#"><img src="img/9.png" class="icons" title="Facebook" class="tooltip"></a>
+					<a href="#"><img src="img/10.png" class="icons" title="Twitter" class="tooltip"></a>
+					<a href="#"><img src="img/11.png" class="icons" title="Google Plus" class="tooltip"></a>
+					<a href="#"><img src="img/12.png" class="icons" title="Dribbble" class="tooltip"></a>
+					<a href="#"><img src="img/13.png" class="icons" title="Behance" class="tooltip"></a>
+			 	 </div>
+				<div class="col-md-3"> </div>
+			</div>
+      	</center>
+      </div>
+  </div> 
+  <br />
+    <div class="blog">
+    	<div class="container">
+    		<div class="row">
+    			<div class="col-md-1"> </div>
+		  		<div class="col-md-7">
+		  			<div class="blogs">
+						<h3>Travel</h3>
+						<p>From apartments and rooms to treehouses and boats: stay in unique spaces in 192 countries.</p>
+						<p><a href="#" class="boxB">See how to travel on Airbnb</a></p>
+					</div>
+		  			<div class="blogs">
+						<h3>Travel</h3>
+						<p>From apartments and rooms to treehouses and boats: stay in unique spaces in 192 countries.</p>
+						<p><a href="#" class="boxB">See how to travel on Airbnb</a></p>
+					</div>
+		  			<div class="blogs">
+						<h3>Travel</h3>
+						<p>From apartments and rooms to treehouses and boats: stay in unique spaces in 192 countries.</p>
+						<p><a href="#" class="boxB">See how to travel on Airbnb</a></p>
+					</div>
+		  			<div class="blogs">
+						<h3>Travel</h3>
+						<p>From apartments and rooms to treehouses and boats: stay in unique spaces in 192 countries.</p>
+						<p><a href="#" class="boxB">See how to travel on Airbnb</a></p>
+					</div>
+				</div>
+				<div class="col-md-3">
+					<div class="navright">
+						<center><h3>@jonaranola</h3>
+							<img src="img/jona.png" style="width: 130px; height: auto; opacity: 0.8;">
+						<p>From apartments and rooms to treehouses and boats: stay in unique spaces in 192 countries.</p>
+						<p><a href="#" class="boxA">See how to travel on Airbnb</a></p></center>
+					</div>
+				</div>
+				<div class="col-md-1"> </div>
+    		</div>
+	  </div>
+    </div>
+  </div> 
+  <br/>
+<div id="nav-wrapper">
+    <div id="nav" class="nav">
+    	<div class="navbar-inner">
+	      <div class="container">
+	        <ul class="pull-left">
+	          <li><a href="#"><font style="font-family: lobster; text-transform: lowercase; font-size: 18px;">jonaranola</font></a></li>
+	          <li><a href="#">Browse</a></li>
+	        </ul>
+	        <ul class="pull-right">
+	          <li><a href="#">Blogs</a></li>
+	          <li><a href="#">Gallery</a></li>
+	          <li><a href="#">Resume</a></li>
+	        </ul>
+	    </div>
+	  </div>
+    </div>
+</div>
+
+
+    <div class="learn-more">
+	  <div class="container">
+		<div class="row">
+  <br />
+	  	  <div class="col-md-3">
+	  	  	<div class="gallery">
+	  	  		<center>
+	  	  			<img src="img/nav.png">
+					<h3>Travel</h3>
+					<p>From apartments and rooms to treehouses and boats: stay in unique spaces in 192 countries.</p>
+					<p><a href="#" class="boxA">See how to travel on Airbnb</a></p>
+				</center>
+			</div>
+		  </div>
+		  <div class="col-md-3">
+	  	  	<div class="gallery">
+		  	  	<center>
+		  	  		<img src="img/nav.png">
+					<h3>Travel</h3>
+					<p>From apartments and rooms to treehouses and boats: stay in unique spaces in 192 countries.</p>
+					<p><a href="#" class="boxA">See how to travel on Airbnb</a></p>
+				</center>
+			</div>
+	      </div>
+		  <div class="col-md-3">
+	  	  	<div class="gallery">
+		  	  	<center>
+		  	  		<img src="img/nav.png">
+					<h3>Travel</h3>
+					<p>From apartments and rooms to treehouses and boats: stay in unique spaces in 192 countries.</p>
+					<p><a href="#" class="boxA">See how to travel on Airbnb</a></p>
+				</center>
+			</div>
+		</div>
+		  <div class="col-md-3">
+	  	  	<div class="gallery">
+		  	  	<center>
+		  	  		<img src="img/nav.png">
+					<h3>Travel</h3>
+					<p>From apartments and rooms to treehouses and boats: stay in unique spaces in 192 countries.</p>
+					<p><a href="#" class="boxA">See how to travel on Airbnb</a></p>
+				</center>
+			</div>
+		  </div>
+
+
+	    </div>
+	  </div>
+	</div>
+    <div class="resume">
+    	<div class="container">
+    		<div class="row">
+		  		<div class="col-md-4">
+		  			<div class="resumeOn">
+						<h3>Jona Marie Dueñas Rañola</h3>
+						<p>BS Computer Engineering Student</p>
+						<br />
+						<p><b>Personal Information</b></p>
+						<p>18 years old (1995)</p>
+						<p>Kapitagan St., Manggahan, Pasig City</p>
+						<p>jmranola@yahoo.com</p>
+						<br />
+
+						<p><b>Working Experience</b></p>
+						<p>Internship</p>
+						<p>Information and Communication Technology Center</p>
+						<p>April - June 2014</p>
+						<br />
+						<p>Freelancer</p>
+						<p>Site Layouts</p>
+						<p>Since 2014</p>
+						<br />
+						<p>Freelancer - Digital Artist</p>
+						<p>Different offline gaming sites</p>
+						<p>Since 2009</p>
+
+						</div>
+					</div>
+		  		<div class="col-md-6">
+		  			<div class="resumeOn">
+						<br />
+						<p><b>Skills</b></p>
+						<p>HTML5/CSS</p>
+						<p>User Interface Designs</p>
+						<p>Adobe Photoshop - Advanced</p>
+						<p>Vectoring/ Paths - Knowledgeable</p>
+						<p>Javascript/ JQuery UI Basics</p>
+						<p>Java and C++ Basics</p>
+						<p>Some Electronics</p>
+						<br />
+
+						<p><b>Software</b></p>
+						<p>Adobe Photoshop</p>
+						<p>Adobe Illustrator</p>
+						<p>Eclipse</p>
+						<p>Sublime and Notepad++</p>
+						<p>XAMPP</p>
+						<p><b>Education</b></p>
+						<p>2011- present: Polytechnic University of the Philippines - Sta. Mesa</p>
+						<p>2007-2011: Manggahan High School</p>
+						<p>2001-2007: San Lorenzo Ruiz Elementary School</p>
+
+				</div>
+				</div>
+		  		<div class="col-md-2">
+		  			<div class="orangeShade-one"></div>
+		  			<div class="orangeShade-two"></div>
+		  			<div class="orangeShade-three"></div>
+		  			<div class="orangeShade-four"></div>
+		  			<div class="orangeShade-five"></div>
+		  			<div class="orangeShade-six"></div>
+		  			<div class="orangeShade-seven"></div>
+				</div>
+				<div class="col-md-1"> </div>
+    		</div>
+	  </div>
+    </div>
+  </div> 
+  <br />
+	</body>
+
+</html>
